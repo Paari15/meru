6a89213cbc904da087a19ab746bf02334aa5638a
PAARI C <166461211+Paari15@users.noreply.github.com>
2025-03-20 14:54:43 +0530

    Update build.ts

--- scripts/build.ts
+++ scripts/build.ts
@@ -5,113 +5,113 @@ import { type Subprocess, build, file, serve, spawn } from "bun";
 import bunPluginTailwind from "bun-plugin-tailwind";
 
 const args = parseArgs({
-	args: Bun.argv,
-	options: {
-		dev: {
-			type: "boolean",
-		},
-	},
-	strict: true,
-	allowPositionals: true,
+    args: Bun.argv,
+    options: {
+        dev: {
+            type: "boolean",
+        },
+    },
+    strict: true,
+    allowPositionals: true,
 });
 
 function cleanup() {
-	return Promise.all(
-		["./out", "./dist"].map((dir) => rm(dir, { recursive: true, force: true })),
-	);
+    return Promise.all(
+        ["./out", "./dist"].map((dir) => rm(dir, { recursive: true, force: true })),
+    );
 }
 
 function buildAppFiles() {
-	build({
-		entrypoints: [
-			"./src/app.ts",
-			"./src/gmail/preload/index.ts",
-			"./src/renderer/preload.ts",
-		],
-		outdir: "./out",
-		target: "node",
-		format: "cjs",
-		external: ["electron"],
-		sourcemap: "linked",
-		define: !args.values.dev
-			? {
-					"process.env.NODE_ENV": JSON.stringify("production"),
-				}
-			: undefined,
-	});
+    build({
+        entrypoints: [
+            "./src/app.ts",
+            "./src/gmail/preload/index.ts",
+            "./src/renderer/preload.ts",
+        ],
+        outdir: "./out",
+        target: "node",
+        format: "esm",
+        external: ["electron"],
+        sourcemap: "linked",
+        define: !args.values.dev
+            ? {
+                "process.env.NODE_ENV": JSON.stringify("production"),
+            }
+            : undefined,
+    });
 }
 
 async function buildRenderer() {
-	if (args.values.dev) {
-		return serve({
-			static: { "/": rendererHtml },
-			development: true,
-			fetch: () => Response.json(null),
-		});
-	}
+    if (args.values.dev) {
+        return serve({
+            static: { "/": rendererHtml },
+            development: true,
+            fetch: () => Response.json(null),
+        });
+    }
 
-	await build({
-		entrypoints: ["./src/renderer/index.html"],
-		outdir: "./out/renderer",
-		sourcemap: "linked",
-		define: {
-			"process.env.NODE_ENV": JSON.stringify("production"),
-		},
-		plugins: [bunPluginTailwind],
-	});
+    await build({
+        entrypoints: ["./src/renderer/index.html"],
+        outdir: "./out/renderer",
+        sourcemap: "linked",
+        define: {
+            "process.env.NODE_ENV": JSON.stringify("production"),
+        },
+        plugins: [bunPluginTailwind],
+    });
 
-	const appJs = await file("./out/app.js");
+    const appJs = await file("./out/app.js");
 
-	await appJs.write(
-		await appJs.text().then((text) => text.replace(/var __dirname.*;/g, "")),
-	);
+    await appJs.write(
+        await appJs.text().then((text) => text.replace(/var __dirname.*;/g, "")),
+    );
 }
 
 await Promise.all([cleanup(), buildAppFiles(), buildRenderer()]);
 
 if (args.values.dev) {
-	let electron: Subprocess;
-	let isRestartingElectron = false;
+    let electron: Subprocess;
+    let isRestartingElectron = false;
 
-	const startElectron = () => {
-		electron = spawn(["electron", "."], {
-			onExit: () => {
-				if (!isRestartingElectron) {
-					process.exit(0);
-				}
-			},
-		});
-	};
+    const startElectron = () => {
+        electron = spawn(["electron", "."], {
+            onExit: () => {
+                if (!isRestartingElectron) {
+                    process.exit(0);
+                }
+            },
+        });
+    };
 
-	const stopElectron = async () => {
-		electron.kill();
+    const stopElectron = async () => {
+        electron.kill();
 
-		await electron.exited;
-	};
+        await electron.exited;
+    };
 
-	const restartElectron = async () => {
-		isRestartingElectron = true;
+    const restartElectron = async () => {
+        isRestartingElectron = true;
 
-		await stopElectron();
+        await stopElectron();
 
-		await startElectron();
+        await startElectron();
 
-		isRestartingElectron = false;
-	};
+        isRestartingElectron = false;
+    };
 
-	await startElectron();
+    await startElectron();
 
-	const watcher = watch("./src", { recursive: true });
+    const watcher = watch("./src", { recursive: true });
 
-	for await (const event of watcher) {
-		if (event.filename?.includes("renderer")) {
-			continue;
-		}
+    for await (const event of watcher) {
+        if (event.filename?.includes("renderer")) {
+            continue;
+        }
 
-		await cleanup();
+        await cleanup();
 
-		await buildAppFiles();
+        await buildAppFiles();
 
-		await restartElectron();
-	}
+        await restartElectron();
+    }
 }
